8df2c3d18590fac74d23124c8cd831e75bcfe691
Emile Kroeger <flammifer@gmail.com>
2016-01-12 00:05:53 +0100

    Start working on tile-based display

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,12 @@
  */
 angular.module('metacastleApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.getStyle = function(i, j) {
+      console.debug([i, j]);
+      var style = {left: (16 * i) + 'px'}
+    }
+    $scope.rows = [
+      [1, 2, 3],
+      [4, 5, 6],
     ];
   });
