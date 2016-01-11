1f834bdcb298e55fc27004a00ef610c8e91a35ca
Emile Kroeger <flammifer@gmail.com>
2016-01-11 23:26:23 +0100

    Create basic skeleton app, using Yeoman. For some reason I had to copy all the bower stuff into 'app', so I guess it's duplicated, but hey, it works, so I'm not going to lose any sleep over that.

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name metacastleApp
+ * @description
+ * # metacastleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('metacastleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize'
+  ]);
