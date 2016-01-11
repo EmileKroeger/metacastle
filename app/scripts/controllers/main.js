960a5e76db6c09fdf8d30f06d25a05e7e0d2b9b2
Emile Kroeger <flammifer@gmail.com>
2016-01-12 00:28:50 +0100

    Make a helper to view the whole tileset, with special tile codes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,27 @@
  */
 angular.module('metacastleApp')
   .controller('MainCtrl', function ($scope) {
+    $scope.range = 
+    function range(count) {
+        return Array.apply(0, Array(count))
+                    .map(function (element, index) { 
+                             return index;  
+                         });
+    }
+    
+    function tilePos(tilecode) {
+      var ti = tilecode % 100;
+      var tj = Math.floor((tilecode - ti) / 100);
+      return "-" + (17 * ti) + "px -" + (17 * tj) + "px";
+    }
+    
     $scope.getStyle = function(i, j) {
-      console.debug([i, j]);
-      var style = {left: (16 * i) + 'px'}
+      var style = {
+        left: (16 * i) + 'px',
+        top: (16 * j) + 'px',
+        "background-position": tilePos(100 * j + i),
+      };
+      return style;
     }
     $scope.rows = [
       [1, 2, 3],
